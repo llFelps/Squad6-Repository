9c664190fceff9de86a78653bb9d9580d4c1d34f
André Alves de Lima <alveslimaandre@gmail.com>
2024-06-24 18:13:18 -0300

    Envio da Branch Andre

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+operacao = input("Digite a operação (soma, subtracao, multiplicacao, divisao): ").lower()
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
 
+match operacao:
+    case 'soma':
+        resultado = num1 + num2
+    case 'subtracao':
+        resultado = num1 - num2
+    case 'multiplicacao':
+        resultado = num1 * num2
+    case 'divisao':
+        resultado = num1 / num2
+    case _:
+        resultado = 'Operação inválida'
+
+
+print(f"Resultado da {operacao}: {resultado}")
